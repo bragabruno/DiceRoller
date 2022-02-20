0bbe520ecade94cf9e2d0107b9b0c1d155ffd5aa
brunobraga <braga.brun81@gmail.com>
2022-02-20 06:57:34 -0600

    Comments added to the MainActivity.kt

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,10 +12,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Line below gets the layout UI through findViewById and stores in and val rollButton
+        // and type Button(Button.class) val rollButton
+        // public class AppCompatActivity extends FragmentActivity implements AppCompatCallback,
+        // TaskStackBuilder.SupportParentable, ActionBarDrawerToggle.DelegateProvider {
         val rollButton: Button = findViewById(R.id.roll_button)
+
+        /**
+        * View.setOnClickListener Interface
+        * Instance creation of anonymous class implements in the curly braces body
+        * new View.OnClickListener()
+        * Any class that implements View.OnClickListener must
+        * implement the methods declared in it (e.g. onClick)
+        *
+        * setOnClickListener just saves the reference to the
+        * View.OnClickListener instance you supplied, and when
+        * someone clicks the button, the onClick method of the
+        * listener you set is getting called.
+        */
         rollButton.setOnClickListener {
             rollDice()
-//            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -24,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
         val randomInt = Random().nextInt(6)+1
 
+        // .text = SetText method from TextView class
         resultText.text = randomInt.toString()
-//        TODO("Not yet implemented")
     }
 }
