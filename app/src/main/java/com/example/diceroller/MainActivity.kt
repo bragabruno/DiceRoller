5c98f094ee6c95c269dba7bec3d695066436b7bf
brunobraga <braga.brun81@gmail.com>
2022-02-20 07:32:36 -0600

    replace the TextView with an ImageView that shows the right drawable based on the random number.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -12,39 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Line below gets the layout UI through findViewById and stores in and val rollButton
-        // and type Button(Button.class) val rollButton
-        // public class AppCompatActivity extends FragmentActivity implements AppCompatCallback,
-        // TaskStackBuilder.SupportParentable, ActionBarDrawerToggle.DelegateProvider {
         val rollButton: Button = findViewById(R.id.roll_button)
-
-        /**
-        * View.setOnClickListener Interface
-        * Instance creation of anonymous class implements in the curly braces body
-        * new View.OnClickListener()
-        * Any class that implements View.OnClickListener must
-        * implement the methods declared in it (e.g. onClick)
-        *
-        * setOnClickListener just saves the reference to the
-        * View.OnClickListener instance you supplied, and when
-        * someone clicks the button, the onClick method of the
-        * listener you set is getting called.
-        */
         rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        // findViewById to get a reference to the TextView and assign
-        // it to an immutable variable called resultText:
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        // have the rollDice method to get a random number between 1 and 6:
         val randomInt = Random().nextInt(6)+1
+        val drawableResource = when (randomInt) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-        // .text = SetText method from TextView class
-        // set the random value that you got above as the text of the TextView:
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(drawableResource)
+
+
     }
 }
