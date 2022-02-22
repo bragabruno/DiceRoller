57738a495ad6aca0995d191674c29ce2b883bcbd
brunobraga <braga.brun81@gmail.com>
2022-02-22 13:12:17 -0600

    add tools:src to ImageView in activity_main.xml

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,11 +10,12 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    // lateinit modififer keyword promises the Kotlin compiler that
-    // the variable will be initialized before calling
-    // any operations on it. So won't be null.
-    // var diceImge: ImageView? = null <- instead this line
-    // that require null track in every call of the field diceImage
+    /** lateinit modififer keyword promises the Kotlin compiler that
+     * the variable will be initialized before calling
+     * any operations on it. So won't be null.
+     * var diceImge: ImageView? = null <- instead this line
+     * that require null track in every call of the field diceImage
+     */
     lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,15 +29,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6)+1
+        val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
         diceImage.setImageResource(drawableResource)
 
     }
